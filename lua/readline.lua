5e1caf4fcd1e7e88cad3ad1262dee7380381504d
Andrey Mishchenko <mishchea@gmail.com>
2022-05-18 10:36:27 -0600

    Use word_chars instead of skip_chars

--- lua/readline.lua
+++ lua/readline.lua
@@ -1,11 +1,14 @@
-local M = {}
+local readline = {}
 
-M.skip_chars = {
+local alphanum = 'abcdefghijklmnopqrstuvwxyz' ..
+                 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ..
+                 '0123456789'
+
+readline.word_chars = {
+  c = alphanum .. '_',
 }
 
-local whitespace = ' 	'
-
-local function new_cursor(s, i, dir, skip_chars)
+local function new_cursor(s, i, dir, word_chars)
   local length = vim.fn.strchars(s)
   local next_char_idx = function(j)
     return (dir == -1) and (j - 1) or j
@@ -17,7 +20,7 @@ local function new_cursor(s, i, dir, skip_chars)
   local consumed_anything = false
   while can_advance(i) do
     local c = vim.fn.nr2char(vim.fn.strgetchar(s, next_char_idx(i)))
-    if string.find(skip_chars, c, 1, true) then
+    if not string.find(word_chars, c, 1, true) then
       if consumed_anything then
         break
       end
@@ -29,15 +32,15 @@ local function new_cursor(s, i, dir, skip_chars)
   return i
 end
 
-local function forward_word_cursor(s, i, skip_chars)
-  return new_cursor(s, i, 1, skip_chars)
+local function forward_word_cursor(s, i, word_chars)
+  return new_cursor(s, i, 1, word_chars)
 end
 
-local function backward_word_cursor(s, i, skip_chars)
-  return new_cursor(s, i, -1, skip_chars)
+local function backward_word_cursor(s, i, word_chars)
+  return new_cursor(s, i, -1, word_chars)
 end
 
-function M._run_tests()
+function readline._run_tests()
   local messages = {}
   local function pl(s) table.insert(messages, {s .. '\n'}) end
   pl('Running tests')
@@ -52,54 +55,54 @@ function M._run_tests()
 
   do
     pl('Testing forward_word_cursor')
-    assert_ints_equal(forward_word_cursor('hello', 0, whitespace), 5)
-    assert_ints_equal(forward_word_cursor('a b c', 0, whitespace), 1)
-    assert_ints_equal(forward_word_cursor('a b c', 1, whitespace), 3)
-    assert_ints_equal(forward_word_cursor('a b c', 2, whitespace), 3)
-    assert_ints_equal(forward_word_cursor('a b c', 3, whitespace), 5)
-    assert_ints_equal(forward_word_cursor('a b c', 4, whitespace), 5)
-    assert_ints_equal(forward_word_cursor('a b c', 5, whitespace), 5)
-    assert_ints_equal(forward_word_cursor('  ', 0, whitespace), 2)
-    assert_ints_equal(forward_word_cursor('  ', 1, whitespace), 2)
-    assert_ints_equal(forward_word_cursor('  ', 2, whitespace), 2)
-    assert_ints_equal(forward_word_cursor(' x ', 0, whitespace), 2)
-    assert_ints_equal(forward_word_cursor(' x ', 1, whitespace), 2)
-    assert_ints_equal(forward_word_cursor(' x ', 2, whitespace), 3)
-    assert_ints_equal(forward_word_cursor(' x ', 3, whitespace), 3)
-    assert_ints_equal(forward_word_cursor('xx ', 0, whitespace), 2)
-    assert_ints_equal(forward_word_cursor('xx ', 1, whitespace), 2)
-    assert_ints_equal(forward_word_cursor('xx ', 2, whitespace), 3)
-    assert_ints_equal(forward_word_cursor('xx ', 3, whitespace), 3)
-    assert_ints_equal(forward_word_cursor(' xx', 0, whitespace), 3)
-    assert_ints_equal(forward_word_cursor(' xx', 1, whitespace), 3)
-    assert_ints_equal(forward_word_cursor(' xx', 2, whitespace), 3)
-    assert_ints_equal(forward_word_cursor(' xx', 3, whitespace), 3)
+    assert_ints_equal(forward_word_cursor('hello', 0, alphanum), 5)
+    assert_ints_equal(forward_word_cursor('a b c', 0, alphanum), 1)
+    assert_ints_equal(forward_word_cursor('a b c', 1, alphanum), 3)
+    assert_ints_equal(forward_word_cursor('a b c', 2, alphanum), 3)
+    assert_ints_equal(forward_word_cursor('a b c', 3, alphanum), 5)
+    assert_ints_equal(forward_word_cursor('a b c', 4, alphanum), 5)
+    assert_ints_equal(forward_word_cursor('a b c', 5, alphanum), 5)
+    assert_ints_equal(forward_word_cursor('  ', 0, alphanum), 2)
+    assert_ints_equal(forward_word_cursor('  ', 1, alphanum), 2)
+    assert_ints_equal(forward_word_cursor('  ', 2, alphanum), 2)
+    assert_ints_equal(forward_word_cursor(' x ', 0, alphanum), 2)
+    assert_ints_equal(forward_word_cursor(' x ', 1, alphanum), 2)
+    assert_ints_equal(forward_word_cursor(' x ', 2, alphanum), 3)
+    assert_ints_equal(forward_word_cursor(' x ', 3, alphanum), 3)
+    assert_ints_equal(forward_word_cursor('xx ', 0, alphanum), 2)
+    assert_ints_equal(forward_word_cursor('xx ', 1, alphanum), 2)
+    assert_ints_equal(forward_word_cursor('xx ', 2, alphanum), 3)
+    assert_ints_equal(forward_word_cursor('xx ', 3, alphanum), 3)
+    assert_ints_equal(forward_word_cursor(' xx', 0, alphanum), 3)
+    assert_ints_equal(forward_word_cursor(' xx', 1, alphanum), 3)
+    assert_ints_equal(forward_word_cursor(' xx', 2, alphanum), 3)
+    assert_ints_equal(forward_word_cursor(' xx', 3, alphanum), 3)
   end
 
   do
     pl('Testing backward_word_cursor')
-    assert_ints_equal(backward_word_cursor('hello', 5, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('a b c', 0, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('a b c', 1, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('a b c', 2, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('a b c', 3, whitespace), 2)
-    assert_ints_equal(backward_word_cursor('a b c', 4, whitespace), 2)
-    assert_ints_equal(backward_word_cursor('a b c', 5, whitespace), 4)
-    assert_ints_equal(backward_word_cursor('  ', 0, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('  ', 1, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('  ', 2, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' x ', 0, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' x ', 1, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' x ', 2, whitespace), 1)
-    assert_ints_equal(backward_word_cursor(' x ', 3, whitespace), 1)
-    assert_ints_equal(backward_word_cursor('xx ', 0, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('xx ', 1, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('xx ', 2, whitespace), 0)
-    assert_ints_equal(backward_word_cursor('xx ', 3, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' xx', 0, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' xx', 1, whitespace), 0)
-    assert_ints_equal(backward_word_cursor(' xx', 2, whitespace), 1)
-    assert_ints_equal(backward_word_cursor(' xx', 3, whitespace), 1)
+    assert_ints_equal(backward_word_cursor('hello', 5, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('a b c', 0, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('a b c', 1, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('a b c', 2, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('a b c', 3, alphanum), 2)
+    assert_ints_equal(backward_word_cursor('a b c', 4, alphanum), 2)
+    assert_ints_equal(backward_word_cursor('a b c', 5, alphanum), 4)
+    assert_ints_equal(backward_word_cursor('  ', 0, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('  ', 1, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('  ', 2, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' x ', 0, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' x ', 1, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' x ', 2, alphanum), 1)
+    assert_ints_equal(backward_word_cursor(' x ', 3, alphanum), 1)
+    assert_ints_equal(backward_word_cursor('xx ', 0, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('xx ', 1, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('xx ', 2, alphanum), 0)
+    assert_ints_equal(backward_word_cursor('xx ', 3, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' xx', 0, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' xx', 1, alphanum), 0)
+    assert_ints_equal(backward_word_cursor(' xx', 2, alphanum), 1)
+    assert_ints_equal(backward_word_cursor(' xx', 3, alphanum), 1)
   end
 
   vim.api.nvim_echo(messages, true, {})
@@ -114,12 +117,18 @@ local function last_cursor_col()
   return vim.fn.strchars(vim.fn.getline '.')
 end
 
+local function get_word_chars()
+  return readline.word_chars[vim.bo.filetype]
+      or vim.b.readline_word_chars
+      or readline.word_chars['c']
+end
+
 local function forward_cursor_col()
-  return forward_word_cursor(vim.fn.getline '.', cursor_col(), vim.o.breakat)
+  return forward_word_cursor(vim.fn.getline '.', cursor_col(), get_word_chars())
 end
 
 local function backward_cursor_col()
-  return backward_word_cursor(vim.fn.getline '.', cursor_col(), vim.o.breakat)
+  return backward_word_cursor(vim.fn.getline '.', cursor_col(), get_word_chars())
 end
 
 local function move_cursor(new_cursor_col)
@@ -136,36 +145,36 @@ local function kill_text(cursor_1, cursor_2)
   vim.api.nvim_buf_set_text(0, line - 1, cursor_start, line - 1, cursor_end, {})
 end
 
-function M.forward_word()
+function readline.forward_word()
   move_cursor(forward_cursor_col())
 end
 
-function M.backward_word()
+function readline.backward_word()
   move_cursor(backward_cursor_col())
 end
 
-function M.end_of_line()
+function readline.end_of_line()
   move_cursor(last_cursor_col())
 end
 
-function M.beginning_of_line()
+function readline.beginning_of_line()
   move_cursor(0)
 end
 
-function M.kill_word()
+function readline.kill_word()
   kill_text(cursor_col(), forward_cursor_col())
 end
 
-function M.backward_kill_word()
+function readline.backward_kill_word()
   kill_text(cursor_col(), backward_cursor_col())
 end
 
-function M.kill_line()
+function readline.kill_line()
   kill_text(cursor_col(), last_cursor_col())
 end
 
-function M.backward_kill_line()
+function readline.backward_kill_line()
   kill_text(cursor_col(), 0)
 end
 
-return M
+return readline
