04d389b1e99285dee5a4dc0315fde5127085c385
Andrey Mishchenko <mishchea@gmail.com>
2022-05-18 08:04:36 -0600

    Stub out unit tests and main interface

--- lua/readline.lua
+++ lua/readline.lua
@@ -1,2 +1,35 @@
 local M = {}
+
+function M._run_tests()
+  local messages = {}
+  local function pl(s) table.insert(messages, {s .. '\n'}) end
+  pl('Running tests')
+  pl('All tests passed!')
+  vim.api.nvim_echo(messages, true, {})
+end
+
+function M.forward_word()
+end
+
+function M.backward_word()
+end
+
+function M.beginning_of_line()
+end
+
+function M.end_of_line()
+end
+
+function M.kill_word()
+end
+
+function M.backward_kill_word()
+end
+
+function M.kill_line()
+end
+
+function M.backward_kill_line()
+end
+
 return M
